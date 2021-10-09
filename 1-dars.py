e8133ac511746a5a9e14632eae6903526bb996a1
Javlon330 <92207619+Javlon330@users.noreply.github.com>
2021-10-09 21:16:59 +0500

    Python darslari
    yangi  codelar shu yerda

---
+++ 1-dars.py
@@ -0,0 +1,10 @@
+# foydalanuvchilar = ['anvar','rustam','javlon','bekzod','arslon']
+# login = input('Login kiriting :')
+# if login.lower() in foydalanuvchilar:
+#     print('Login band , yangi login kiriting') 
+# else: 
+#     print('Xush kelibsiz')
+# x = int(input('Butun son kiriting : '))
+# for i in range(2,11):
+#     if not (x%i):
+#         print(f"{x} soni {i} ga qoldiqsiz bolinadi" )
