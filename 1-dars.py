99197a8c89c6243b637d4046b2a3eea628f9e5d0
Javlon330 <92207619+Javlon330@users.noreply.github.com>
2021-10-10 21:09:22 +0500

    Update 1-dars.py

--- 1-dars.py
+++ 1-dars.py
@@ -27,12 +27,37 @@ python_il = {
     'if_else' : 'shart opertorlari',
     'list' : "royhat",
     'dictionary' : "lugat",
-
+    'len' : 'royxat uzunliki',
+    'sort' : "tartiblaydi"
     }
-soz = input("soz kiriting : " )
-# print(python_il.get(soz,'bunday soz mavjud mas'))
-kalit = python_il.get(soz)
-if kalit == None:
-    print('bunday soz yoq')
-else :
-    print(f"{soz} {kalit} deb tarjima qilinadi")
+# soz = input("soz kiriting : " )
+# # print(python_il.get(soz,'bunday soz mavjud mas'))
+# kalit = python_il.get(soz)
+# if kalit == None:
+#     print('bunday soz yoq')
+# else :
+#     print(f"{soz} {kalit} deb tarjima qilinadi")
+# for k,q in sorted(python_il.items()):
+#     print(f"{k} - {q}")
+
+# countries = {
+#     'Fransiya': 'Parij',
+#     'Italiya' : 'Rim',
+#     'Shetsiya': 'Stokgolm',
+#     'Polsha': 'Varshara',
+#     'Avstraliya' : 'Vena',
+#     'Turkiya' : 'Anqara',
+#     'Turkmaniston' : 'Ashxobod'
+#     }
+# print("Dunyo davlatlari")
+# for d in sorted(countries.keys()):
+#     print(d.upper())
+# print(' \n Davlatlarning poytaxtlari')
+# for p in sorted(countries.values()):
+# #     print(p.title())
+# davlat = input("Davlatni nomini kiriting : ")
+# poytaxt = countries.get(davlat)
+# if poytaxt == None:
+#     print("Bunday davlat haqida ma'lumot yoq")
+# else :
+#     print(f"{davlat.upper()} ning poytaxti {poytaxt.title()}")
