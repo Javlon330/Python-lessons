7d66b0e63b7ee8510fd9d258a22ff4cbc8cb1569
Javlon330 <92207619+Javlon330@users.noreply.github.com>
2021-10-10 23:36:10 +0500

    Update 1-dars.py

--- 1-dars.py
+++ 1-dars.py
@@ -20,16 +20,16 @@
 #     'akam' : 'lagmon'
 # }
 # print(f" otamning sevimli taomi {taomlar['otam']} \n onamning sevimli taomi {taomlar['onam']}")
-python_il = {
-    'int': 'butun son tipi', 
-    'float': 'onlik sonlar',
-    'for':'sikl operatori',
-    'if_else' : 'shart opertorlari',
-    'list' : "royhat",
-    'dictionary' : "lugat",
-    'len' : 'royxat uzunliki',
-    'sort' : "tartiblaydi"
-    }
+# python_il = {
+#     'int': 'butun son tipi', 
+#     'float': 'onlik sonlar',
+#     'for':'sikl operatori',
+#     'if_else' : 'shart opertorlari',
+#     'list' : "royhat",
+#     'dictionary' : "lugat",
+#     'len' : 'royxat uzunliki',
+#     'sort' : "tartiblaydi"
+#     }
 # soz = input("soz kiriting : " )
 # # print(python_il.get(soz,'bunday soz mavjud mas'))
 # kalit = python_il.get(soz)
@@ -61,23 +61,121 @@ python_il = {
 #     print("Bunday davlat haqida ma'lumot yoq")
 # else :
 #     print(f"{davlat.upper()} ning poytaxti {poytaxt.title()}") 
-menu = {
-    'shashlik':15000,
-    'qatlet':24000,
-    'tabaqa': 45000,
-    'manti' : 12000,
-    'baliq' : 55000,
-    'somsa' : 7000,
-    'barak' : 17000
+# menu = {
+#     'shashlik':15000,
+#     'qatlet':24000,
+#     'tabaqa': 45000,
+#     'manti' : 12000,
+#     'baliq' : 55000,
+#     'somsa' : 7000,
+#     'barak' : 17000
+# }
+# taom = []
+# print('3 ta taom buyurtma qiling')
+# for i in range(3):
+#     taom.append( input(f"{i+1} taom : ").lower())
+# for zakaz in taom:
+#     if zakaz in menu:
+#         print(f"{zakaz} {menu[zakaz]} so'm ")
+#     else:
+        # print(f"Kechirasiz bizda {zakaz} yo'q ")
+# shoir1 = {
+#     'ismi' : 'Abu Abdulloh Muhammad ibn Ismiol',
+#     't_yili' : 810,
+#     't_joyi' : 'Buxoro',
+#     'y_yili' : 60,
+#     'asarlari':{ 'al jome as sahih','al adab al mufrat','at tarix al kabir'}
+# }
+# shoir2 = {
+#     'ismi' : 'Abdulla Qodiriy',
+#     't_yili' : 1894,
+#     't_joyi' : 'Toshkent',
+#     'y_yili' : 44,
+#     'asarlari':{ 'al jome as sahih','al adab al mufrat','at tarix al kabir'}
+# }
+# shoir3 = {
+#     'ismi' : 'Erkin Vohidov',
+#     't_yili' : 1936,
+#     't_joyi' : 'Fargona',
+#     'y_yili' : 80,
+#     'asarlari':{ 'al jome as sahih','al adab al mufrat','at tarix al kabir'}
+# }
+# shoir4 = {
+#     'ismi' : 'Alisher Navoiy',
+#     't_yili' : 1441,
+#     't_joyi' : 'Xirot',
+#     'y_yili' : 60,
+#     'asarlari':{ 'al jome as sahih','al adab al mufrat','at tarix al kabir'}
+# }
+# allomalar = [shoir1,shoir2,shoir3,shoir4]
+# # for alloma in allomalar:
+# #     ismi = alloma['ismi']
+# #     t_yili = alloma['t_yili']
+# #     t_joyi = alloma['t_joyi']
+# #     y_yili = alloma['y_yili']
+# #     print(f"\n {ismi} {t_yili}-yilda {t_joyi}da tavallud topgan.{y_yili} yil umir korgan   ")
+# # for i in range(4):
+# #     print(
+# #     f"\n {allomalar[i]['ismi']} {allomalar[i]['t_yili']}-yilda {allomalar[i]['t_joyi']}da tavallud topgan.{allomalar[i]['y_yili']} yil umir korgan   ")
+# for alloma in allomalar:
+#     ismi = alloma['ismi']
+#     print(f"\n{ismi}ning mashhur asarlari: ")
+#     # asar = alloma['asarlari']
+#     for asar in alloma['asarlari']:
+#         print(asar)
+# dostlar ={
+#     'Ali' : ['Terminator','rambo','terminator'],
+#     'Vali' : ['sdads','SFDAD','SDSASAJJ'],
+#     'Rustam' : ['adsda','sdadas','safs']
+# }
+# for ism,kinolar in dostlar.items():
+#     print(f"{ism}ning sevimli kinolar:")
+#     for kino in kinolar:
+#         print(kino)
+
+davlatlar = {
+    "O'zbekiston" : {
+        'Poytaxti' : 'Toshkent',
+        'Hududi' : 448978,
+        'Aholisi' : 30000000,
+        "Pul birligi" : "so'm"
+    },
+    "Rossiya" : {
+        'Poytaxti' : 'Moskva',
+        'Hududi' :17980746,
+        'Aholisi' : 144000000,
+        "Pul birligi" : "rubl"
+    },
+    "AQSH" : {
+        'Poytaxti' : 'Vashington',
+        'Hududi' : 92432423,
+        'Aholisi' : 327000000,
+        "Pul birligi" : "dollor"
+    }
 }
-taom = []
-print('3 ta taom buyurtma qiling')
-for i in range(3):
-    taom.append( input(f"{i+1} taom : ").lower())
-for zakaz in taom:
-    if zakaz in menu:
-        print(f"{zakaz} {menu[zakaz]} so'm ")
-    else:
-        print(f"Kechirasiz bizda {zakaz} yo'q ")
+davlat = input("Davlat nomini kiriting :")
+if davlat in davlatlar:
+    info = davlatlar[davlat]
+    print(
+        f" {davlat.upper()}ning poytaxti {info['Poytaxti']} "
+        f"\n Hududi : {info['Hududi']} "
+        f"\n Aholisi : {info['Aholisi']} "
+        f"\n pul birligi : {info['Pul birligi']} "
+    )
 
 
+
+
+
+# for davlat,korsatkichlar in davlatlar.items():
+#     poytaxt = korsatkichlar['Poytaxti']
+#     hudud = korsatkichlar['Hududi']
+#     aholisi = korsatkichlar['Aholisi']
+#     pul_birligi = korsatkichlar['Pul birligi']
+#     print(
+#         f"   {davlat}ning poytaxti {poytaxt} \n"
+#         f" Hududi  : {hudud} \n"
+#         f" Aholisi : {aholisi} \n "
+#         f" pul birligi : {pul_birligi} \n "
+    
+#     )
