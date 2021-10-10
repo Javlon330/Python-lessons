fb944f47fccc7f734ae67276eaafd84447b07f32
Javlon330 <92207619+Javlon330@users.noreply.github.com>
2021-10-10 15:52:44 +0500

    yangi ozgarish

--- 1-dars.py
+++ 1-dars.py
@@ -9,3 +9,30 @@
 #     if not (x%i):
 #         print(f"{x} soni {i} ga qoldiqsiz bolinadi" )
 # kop uchraydigan hatoliklar
+# # LUG'ATLAR
+# otam = { 'ismi' : 'Sabirjon','yoshi' : 57, "t_joyi" : 'Urganch tumani'}
+# onam = { 'ismi' : 'Sultonposhha','yoshi' : 56, "t_joyi" : 'Urganch shahri'}
+# akam = { 'ismi' : 'Yodgor','yoshi' : 31, "t_joyi" : 'Urganch tumani'}
+# print(f"Otamning ismi {otam['ismi']} yoshi {otam['yoshi']} da {otam['t_joyi']}da tugilgan ")
+# taomlar = {
+#     'otam' : 'osh',
+#     'onam' : "manti",
+#     'akam' : 'lagmon'
+# }
+# print(f" otamning sevimli taomi {taomlar['otam']} \n onamning sevimli taomi {taomlar['onam']}")
+python_il = {
+    'int': 'butun son tipi', 
+    'float': 'onlik sonlar',
+    'for':'sikl operatori',
+    'if_else' : 'shart opertorlari',
+    'list' : "royhat",
+    'dictionary' : "lugat",
+
+    }
+soz = input("soz kiriting : " )
+# print(python_il.get(soz,'bunday soz mavjud mas'))
+kalit = python_il.get(soz)
+if kalit == None:
+    print('bunday soz yoq')
+else :
+    print(f"{soz} {kalit} deb tarjima qilinadi")
